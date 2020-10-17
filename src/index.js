24a62090048676e239e1dbf5924cd780d878afed
arhs-dev <Charis.Giaralis@arhs-dev-hellas.com>
2020-10-17 16:23:05 +0300

    Add docs for user endpoints

--- src/index.js
+++ src/index.js
@@ -7,19 +7,25 @@ const bodyParser = require('body-parser');
 const { usersRouter } = require('./users/users.router');
 const { ensureCollections } = require('./database/main');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 ensureCollections();
 
 const app = express();
 app.use(cors());
 app.use(morgan('dev'));
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(bodyParser.json());
 
 app.use('/movies', moviesRouter);
 app.use('/users', usersRouter);
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(err.status).send(err.message);
+  res.status(err.status).send({ message: err.message });
 });
 
 const server = http.createServer(app);
