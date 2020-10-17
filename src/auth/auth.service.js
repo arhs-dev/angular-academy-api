fd84ee2df5254abef772005f619a1f678ec55f20
arhs-dev <Charis.Giaralis@arhs-dev-hellas.com>
2020-10-17 20:08:54 +0300

    Hardcode secret key inside auth service

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -1,10 +1,9 @@
 const { sign, verify } = require('jsonwebtoken');
-const { config } = require('dotenv');
 
 exports.signJwt = (payload) => {
-  return sign(payload, config().parsed.SECRET_KEY, { expiresIn: '2h' });
+  return sign(payload, 'secret-key', { expiresIn: '2h' });
 };
 
 exports.verifyJwt = (jwt) => {
-  return verify(jwt, config().parsed.SECRET_KEY);
+  return verify(jwt, 'secret-key');
 };
